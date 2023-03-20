556e632d9dc2c8d6a48fc2c906fb90bd9d8ddc84
Runtian Zhu <99370559+rtryanzhu@users.noreply.github.com>
2023-03-20 11:56:36 -0700

    Create getData.py

---
+++ Final Project/getData.py
@@ -0,0 +1,16 @@
+# (API key deleted) alpha vantage api key for exchange rate
+import pandas as pd
+import requests as req
+import time
+import re
+import requests
+import locale
+from selenium import webdriver
+from tqdm import tqdm 
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+from tqdm.notebook import tqdm_notebook
+from collections import Counter
+from itertools import chain
+from requests_html import HTMLSession
+from datetime import datetime, timedelta # time conversion
